a2198dc67ed4c4d485b819058d0a36f541480a2b
Douglas Mendes <mendes-barreto@live.com>
2025-03-04 22:14:57 -0500

    fix: Fix binary output folder

--- citizen.rb
+++ citizen.rb
@@ -1,20 +1,23 @@
 class Citizen < Formula
   desc "Track your citizenship application progress from command line"
   homepage "https://github.com/mendesbarreto/citizenship-tracker-cli"
-  url "https://github.com/mendesbarreto/citizenship-tracker-cli/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  url "https://github.com/mendesbarreto/citizenship-tracker-cli/archive/refs/tags/v0.0.7.tar.gz"
+  sha256 "db61417a9d7b90e7ba1aa03e18e50af5d421b617f17671102168f1ab4cbb2ad4"
   license "MIT"
   head "https://github.com/mendesbarreto/citizenship-tracker-cli.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w")
-    bin.install_symlink "#{bin}/citizenship-tracker-cli" => "citizen"
+    # Build the binary directly to the bin directory
+    system "go", "build", "-o", bin/"citizenship-tracker-cli", "./main.go"
+    
+    # Create a symlink
+    bin.install_symlink bin/"citizenship-tracker-cli" => "citizen"
   end
 
   service do
-    run [opt_bin/"citizenship-tracker-cli"]
+    run [opt_bin/"citizenship-tracker-cli", "run", "--headless"]
     interval 10800 # Run every 3 hours
     log_path var/"log/citizen.log"
     error_log_path var/"log/citizen.error.log"
@@ -23,7 +26,7 @@ class Citizen < Formula
   end
 
   test do
-    output = shell_output("#{bin}/citizen --version")
+    output = shell_output("#{bin}/citizenship-tracker-cli --version")
     assert_match "citizenship-tracker-cli version", output
   end
 end
